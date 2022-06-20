01254c91bf2c691c8f83f5f1a7e6b0c239445ee7
rafmc98 <65073971+rafmc98@users.noreply.github.com>
2022-06-20 20:09:54 +0200

    gestures in match

--- Chess-master/app/src/main/java/com/example/dipanshkhandelwal/chess/MainActivity.java
+++ Chess-master/app/src/main/java/com/example/dipanshkhandelwal/chess/MainActivity.java
@@ -21,6 +21,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -30,12 +31,24 @@ import com.github.bhlangonijr.chesslib.Piece;
 import com.github.bhlangonijr.chesslib.Side;
 import com.github.bhlangonijr.chesslib.Square;
 import com.github.bhlangonijr.chesslib.move.Move;
+import com.google.mediapipe.formats.proto.LandmarkProto;
+import com.google.mediapipe.solutioncore.CameraInput;
+import com.google.mediapipe.solutioncore.SolutionGlSurfaceView;
+import com.google.mediapipe.solutions.hands.HandLandmark;
+import com.google.mediapipe.solutions.hands.Hands;
+import com.google.mediapipe.solutions.hands.HandsOptions;
+import com.google.mediapipe.solutions.hands.HandsResult;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, RecognitionListener {
     public Square c1=null;
     public Square c2=null;
@@ -74,6 +87,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView op3;
     private TextView op4;
 
+
+    private int consecutiveFrames=10;
+    private Hands hands;
+    // Run the pipeline and the model inference on GPU or CPU.
+    private static final boolean RUN_ON_GPU = true;
+    private boolean inFinalCall=false;
+    // Live camera demo UI and camera components.
+    private CameraInput cameraInput;
+
+    private SolutionGlSurfaceView<HandsResult> glSurfaceView;
+
+    private List<ArrayList<Integer>> lmList = new ArrayList<>();
+    private List<Integer> tipIds = new ArrayList<>(Arrays.asList(4, 8, 12, 16, 20));
+    private enum HandOrientation {
+        UNKNOWN,
+        FRONT,
+        BACK
+    }
+    private HandOrientation handOrientation = HandOrientation.UNKNOWN;
+    private Integer gestureCounter = 0;
+    private Integer gestureHolder = null;
+    private Map<Integer, String> gestureMap = new HashMap<Integer, String>() {{
+        put(0, "zero");
+        put(1, "uno");
+        put(2, "due");
+        put(3, "tre");
+        put(4, "quattro");
+        put(5, "cinque");
+        put(6, "rock");
+        put(7, "yo");
+        put(8, "okay");
+    }};
+    private Boolean settingsFlag = false;
 
 
 
@@ -176,6 +222,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     tt.setSpeechRate((float)1);
             }
         });
+        setupLiveDemoUiComponents();
+
     }
 
     @Override
@@ -201,12 +249,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         resetSpeechRecognizer();
         speech.startListening(recognizerIntent);
 
+        // Restarts the camera and the opengl surface rendering.
+        cameraInput = new CameraInput(this);
+        cameraInput.setNewFrameListener(textureFrame -> hands.send(textureFrame));
+        glSurfaceView.post(this::startCamera);
+        glSurfaceView.setVisibility(View.VISIBLE);
+
     }
     @Override
     protected void onPause() {
         Log.i(LOG_TAG, "pause");
         super.onPause();
         speech.stopListening();
+
+        glSurfaceView.setVisibility(View.GONE);
+        cameraInput.close();
     }
     @Override
     protected void onStop() {
@@ -1597,4 +1654,276 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         returnedText.setText(suggestions.getFirstMessage());
         imlistenig.setVisibility(View.GONE);
     }
+
+
+    /** Sets up the UI components for the live demo with camera input. */
+    private void setupLiveDemoUiComponents() {
+        stopCurrentPipeline();
+        setupStreamingModePipeline();
+    }
+
+    /** Sets up core workflow for streaming mode. */
+    private void setupStreamingModePipeline() {
+        // Initializes a new MediaPipe Hands solution instance in the streaming mode.
+        hands =
+                new Hands(
+                        this,
+                        HandsOptions.builder()
+                                .setStaticImageMode(false)
+                                .setMaxNumHands(2)
+                                .setRunOnGpu(RUN_ON_GPU)
+                                .build());
+        hands.setErrorListener((message, e) -> Log.e(LOG_TAG, "MediaPipe Hands error:" + message));
+
+        cameraInput = new CameraInput(this);
+        cameraInput.setNewFrameListener(textureFrame -> hands.send(textureFrame));
+
+        // Initializes a new Gl surface view with a user-defined HandsResultGlRenderer.
+        glSurfaceView =
+                new SolutionGlSurfaceView<>(this, hands.getGlContext(), hands.getGlMajorVersion());
+        glSurfaceView.setSolutionResultRenderer(new HandsResultGlRenderer());
+        glSurfaceView.setRenderInputImage(true);
+
+        hands.setResultListener(
+                handsResult -> {
+
+                    lmList = findPosition(handsResult, 0);
+                    getHandOrientation();
+                    ArrayList<Integer> fingers = fingersUp();
+                    Integer currentGesture = getGestureCode(fingers, handsResult);
+                    if (currentGesture == gestureHolder && currentGesture != null){
+                        if (gestureCounter == consecutiveFrames){
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    View settingsMenu = findViewById(R.id.settingsMenu);
+                                    if (settingsMenu.getVisibility() != View.VISIBLE) {
+                                        if (gestureHolder == 6) cast(findViewById(R.id.screen));
+                                        if (gestureHolder == 7) settings(findViewById(R.id.settings));
+                                        if (gestureHolder == 8) bestmove(findViewById(R.id.bestmove));
+                                    } else {
+                                        if (gestureHolder == 1) restart(findViewById(R.id.restart));
+                                        if (gestureHolder == 2) exit(findViewById(R.id.exit));
+                                        if (gestureHolder == 3) Back(findViewById(R.id.back));
+                                    }
+                                }
+                            });
+                            gestureCounter = 0;
+                            gestureHolder = null;
+                        } else {
+                            gestureCounter ++;
+                        }
+                    } else {
+                        gestureCounter = 0;
+                    }
+                    gestureHolder = currentGesture;
+
+
+                    logWristLandmark(handsResult, /*showPixelValues=*/ false);
+                    glSurfaceView.setRenderData(handsResult);
+                    glSurfaceView.requestRender();
+                });
+
+        // The runnable to start camera after the gl surface view is attached.
+        // For video input source, videoInput.start() will be called when the video uri is available.
+        glSurfaceView.post(this::startCamera);
+
+
+        // Updates the preview layout.
+        FrameLayout frameLayout = findViewById(R.id.preview_display_layout);
+
+        frameLayout.removeAllViewsInLayout();
+        frameLayout.addView(glSurfaceView);
+        // make camera layout invisible
+        glSurfaceView.setVisibility(View.INVISIBLE);
+        frameLayout.requestLayout();
+    }
+
+    private List<ArrayList<Integer>> findPosition(HandsResult result, Integer handNo){
+        List<ArrayList<Integer>> landmarkList= new ArrayList<>();
+        if(!result.multiHandLandmarks().isEmpty()) {
+            LandmarkProto.NormalizedLandmarkList myHand = result.multiHandLandmarks().get(handNo);
+
+            int width = glSurfaceView.getWidth();
+            int height = glSurfaceView.getWidth();
+
+            for (int i = 0; i < myHand.getLandmarkList().size(); i++) {
+
+                int cx = (int) (myHand.getLandmarkList().get(i).getX() * width);
+                int cy = (int) (myHand.getLandmarkList().get(i).getY() * height);
+
+                landmarkList.add(new ArrayList<>(Arrays.asList(i, cx, cy)));
+            }
+        }
+        return landmarkList;
+    }
+
+
+    public Integer getGestureCode(ArrayList<Integer> fingers, HandsResult result){
+
+        if(result.multiHandLandmarks().isEmpty()){
+            return null;
+        }
+        if (fingers.equals(Arrays.asList(0, 0, 0, 0, 0))){
+            return 0;
+        }else{
+            if(fingers.equals(Arrays.asList(0, 1, 0, 0, 0))){
+                return 1;
+            }else{
+                if(fingers.equals(Arrays.asList(0, 1, 1, 0, 0))){
+                    return 2;
+                }else{
+                    if(fingers.equals(Arrays.asList(1, 1, 1, 0, 0))){
+                        return 3;
+                    }else{
+                        if(fingers.equals(Arrays.asList(0, 1, 1, 1, 1))){
+                            return 4;
+                        }else{
+                            if(fingers.equals(Arrays.asList(1, 1, 1, 1, 1))){
+                                return 5;
+                            }else{
+                                if(fingers.equals(Arrays.asList(1, 1, 0, 0, 1))){
+                                    return 6;
+                                }else{
+                                    if(fingers.equals(Arrays.asList(1, 0, 0, 0, 1))){
+                                        return 7;
+                                    }else{
+                                        if(fingers.equals(Arrays.asList(1, 0, 1, 1, 1))){
+                                            return 8;
+                                        } else {
+                                            return null;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void getHandOrientation() {
+        if (!lmList.isEmpty()) {
+            // Check if the x value of 5th landmark is less than the 17th landmark in order to
+            // recognize the hand orientation
+            if (lmList.get(5).get(1) < lmList.get(17).get(1)) {
+                handOrientation = HandOrientation.FRONT;
+            } else {
+                handOrientation = HandOrientation.BACK;
+            }
+        }
+        else {
+            handOrientation = HandOrientation.UNKNOWN;
+        }
+    }
+
+    private ArrayList<Integer> fingersUp(){
+        if(!lmList.isEmpty()){
+            ArrayList<Integer> fingers = new ArrayList<>();
+            if (handOrientation == HandOrientation.FRONT){
+
+                // thumb
+                if (lmList.get(tipIds.get(0)).get(1) < lmList.get(tipIds.get(0) - 1).get(1)) {
+                    fingers.add(1);
+                } else {
+                    fingers.add(0);
+                }
+
+            } else {
+
+                // thumb
+                if (lmList.get(tipIds.get(0)).get(1) > lmList.get(tipIds.get(0) - 1).get(1)) {
+                    fingers.add(1);
+                } else {
+                    fingers.add(0);
+                }
+
+            }
+            // fingers
+            for (int i = 1; i <= 4; i++) {
+                if (lmList.get(tipIds.get(i)).get(2) < lmList.get(tipIds.get(i) - 1).get(2)) {
+                    fingers.add(1);
+                } else {
+                    fingers.add(0);
+                }
+            }
+            return fingers;
+
+        } else {
+            return null;
+        }
+    }
+
+    private void startCamera() {
+        cameraInput.start(
+                this,
+                hands.getGlContext(),
+                CameraInput.CameraFacing.FRONT,
+                glSurfaceView.getWidth(),
+                glSurfaceView.getHeight());
+    }
+
+    private void stopCurrentPipeline() {
+        if (cameraInput != null) {
+            cameraInput.setNewFrameListener(null);
+            cameraInput.close();
+        }
+        try {
+            if (glSurfaceView != null) {
+                glSurfaceView.setVisibility(View.GONE);
+            }
+        } catch (Exception e){
+            // hide the glSurfaceView
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    glSurfaceView.setVisibility(View.GONE);
+                }
+            });
+        }
+        if (hands != null) {
+            hands.close();
+        }
+    }
+
+    private void showToast(Context ctx, String text) {
+        Toast.makeText(ctx, text, Toast.LENGTH_LONG).show();
+    }
+
+    private void logWristLandmark(HandsResult result, boolean showPixelValues) {
+        // If no landmarks are detected
+        if (result.multiHandLandmarks().isEmpty()) {
+            return;
+        }
+
+        LandmarkProto.NormalizedLandmark wristLandmark =
+                result.multiHandLandmarks().get(0).getLandmarkList().get(HandLandmark.WRIST);
+        // For Bitmaps, show the pixel values. For texture inputs, show the normalized coordinates.
+        if (showPixelValues) {
+            int width = result.inputBitmap().getWidth();
+            int height = result.inputBitmap().getHeight();
+            Log.i(
+                    LOG_TAG,
+                    String.format(
+                            "MediaPipe Hand wrist coordinates (pixel values): x=%f, y=%f",
+                            wristLandmark.getX() * width, wristLandmark.getY() * height));
+        } else {
+            Log.i(
+                    LOG_TAG,
+                    String.format(
+                            "MediaPipe Hand wrist normalized coordinates (value range: [0, 1]): x=%f, y=%f",
+                            wristLandmark.getX(), wristLandmark.getY()));
+        }
+        if (result.multiHandWorldLandmarks().isEmpty()) {
+            return;
+        }
+        LandmarkProto.Landmark wristWorldLandmark =
+                result.multiHandWorldLandmarks().get(0).getLandmarkList().get(HandLandmark.WRIST);
+        Log.i(
+                LOG_TAG,
+                String.format(
+                        "MediaPipe Hand wrist world coordinates (in meters with the origin at the hand's"
+                                + " approximate geometric center): x=%f m, y=%f m, z=%f m",
+                        wristWorldLandmark.getX(), wristWorldLandmark.getY(), wristWorldLandmark.getZ()));
+    }
+
 }
